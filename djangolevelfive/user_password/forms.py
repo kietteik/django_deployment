0a9db28883e1280c0e0345889797762813c9ef83
Kiệt <kietteik@host.local>
2019-11-28 16:09:31 +0700

    update regis UI

--- djangolevelfive/user_password/forms.py
+++ djangolevelfive/user_password/forms.py
@@ -4,7 +4,13 @@ from user_password.models import UserProfileInfo
 
 
 class UserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput())
+    def __init__(self, *args, **kwargs):
+        super(UserForm, self).__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({'class': 'username'})
+        self.fields['email'].widget.attrs.update({'class': 'username'})
+
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'password'}))
 
     class Meta:
         model = User
@@ -12,6 +18,13 @@ class UserForm(forms.ModelForm):
 
 
 class UserProfileInfoForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(UserProfileInfoForm, self).__init__(*args, **kwargs)
+        self.fields['website'].label_classes = ('username')
+        # self.fields['profile_pic'].widget.attrs.update(
+        #     {'class': 'login-button'})
+        self.fields['website'].widget.attrs.update({'class': 'username'})
+
     class Meta:
         model = UserProfileInfo
         fields = ('website', 'profile_pic')
